21adee13b72d5097f54cd9125a42bd27c424ff20
Manny <eqo5037@gmail.com>
2024-07-11 08:37:50 +0000

    add docusourus framework

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/bobokeke/Documents/manny/projects/mannyslab/projects/docs/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/bobokeke/Documents/manny/projects/mannyslab/projects/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/bobokeke/Documents/manny/projects/mannyslab/projects/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/bobokeke/Documents/manny/projects/mannyslab/projects/docs/src/css/custom.css"),
+];
